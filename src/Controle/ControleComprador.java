108aa02e06658716f8cdcd113ba81db19867a746
rafae <rafae@RavPCLenovo>
2019-11-20 17:18:38 -0200

    Comprador erro arrumado .

--- src/Controle/ControleComprador.java
+++ src/Controle/ControleComprador.java
@@ -1,7 +1,18 @@
 package Controle;
 
+import Modelo.Comprador;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ControleComprador implements Serializable {
+    // Array do comprador
+
+    ArrayList<Comprador> arrayComprador = new ArrayList<>();
+    // Cria novo comprador e colcoa no array
+
+    void criaComprador(String cpf, String nome, String email, String fone, String contatoPref) {
+        Comprador comprador = new Comprador(cpf, nome, email, fone, contatoPref);
+        arrayComprador.add(comprador);
+    }
 
 }
